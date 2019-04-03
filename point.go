e66903265996a17b3f1fe76e9f9fc52bcb61e31d
Gunnar Þór Magnússon <gunnar@independentview.com>
2019-04-03 13:28:21 +0200

    Split Point into own file

---
+++ point.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Point struct {
+	Name  string
+	Value float32
+}
+
+func PointFromLine(line string) (Point, error) {
+	parts := strings.SplitN(line, " ", 3)
+	if len(parts) != 3 {
+		return Point{}, fmt.Errorf("Line didn't have 3 parts")
+	}
+
+	val, err := strconv.ParseFloat(parts[1], 32)
+	if err != nil {
+		return Point{}, err
+	}
+
+	return Point{parts[0], float32(val)}, nil
+}
+
+func (p Point) StreamName() string {
+	return fmt.Sprintf("metric:%s", p.Name)
+}
+
+func (p Point) Encode() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.LittleEndian, p.Value)
+
+	return buf.Bytes(), err
+}
